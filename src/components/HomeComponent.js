9e80320db68571cdd87782234374ad3a574fd3d5
kuldeepyadavky <ikuldeepyadavky@gmail.com>
2020-07-14 04:19:45 +0530

    redux thunk

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle, CardSubtitle } from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
+function RenderCard({item, isLoading, errMess}){
+	if(isLoading){
+	return(
+		<Loading />
+	);}
+	else if(errMess){
+	return(
+		<h4>{errMess}</h4>
+	);}
+	else {
 
-function RenderCard({item}){
 	return(
 		<Card>
 				<CardImg src={item.image} alt={item.name} />
@@ -15,16 +25,17 @@ function RenderCard({item}){
 	);
 
 }
+}
 
 
 
 function Home(props){
-
 	return(
 		<div className="container">
 				<div className="row align-items-start">
 						<div className="col-12 col-md m-1">
-							<RenderCard item={props.dish} />
+							<RenderCard item={props.dish} isLoading={props.dishesLoading}
+							errMess={props.dishesErrMess} />
 						</div>
 						<div className="col-12 col-md m-1">
 							<RenderCard item={props.promotion} />
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Footer from './FooterComponent';
 import '../App.css'
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -20,17 +21,28 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating , author , comment)=>dispatch(addComment(dishId, rating , author , comment)),
+  fetchDishes: () => {dispatch(fetchDishes())}
+});
+
 
 class Main extends Component {
   constructor(props){
     super(props);
   }
 
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render(){
 
     const HomePage = () => {
       return(
-        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+        <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+        dishesLoading={this.dishes.isLoading}
+        dishesErrMess={this.dishes.errMess}
         promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
         leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
@@ -38,11 +50,14 @@ class Main extends Component {
 
       const DishWithId = ({match}) => {
         return(
-          <DishDetail dish={this.props.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
-           comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))[0]}
+          <DishDetail dish={this.props.dishes.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
+          dishesLoading={this.dishes.isLoading}
+          errMess={this.dishes.errMess}
+           comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+           addComment={this.props.addComment}
           />
         );
-      }
+      };
 
       return (
     <div className="App">
@@ -62,4 +77,4 @@ class Main extends Component {
 }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
